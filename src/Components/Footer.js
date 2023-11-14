ebcd3bc46b2c6e2cf544d0bfe5e2fef0ffef192e
Juan Lopez <juanchonelo2005@hotmail.com>
2023-11-14 00:00:51 -0300

    arreglo de footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
+import { styled } from "@mui/system";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import FacebookIcon from "@mui/icons-material/Facebook";
@@ -24,24 +24,23 @@ const Item = styled("div")(({ theme }) => ({
 
 export default function StickyFooter() {
   return (
-    <Box className="footer" sx={{position: "static",
-    marginTop: "28vh",
-    height: "6vh",
-    bottom: 0,
-    width: "100vw"
+    <Box className="footer" sx={{
+      position: "fixed",
+      bottom: 0,
+      width: "100%",
     }}>
       <Grid container spacing={2}>
-        <Grid item xs={6} sx={{fontSize: "1.2rem", position: "relative",  left: "-2rem", top: "-.3rem"}}>
+        <Grid item xs={6} sx={{ fontSize: "1.2rem", position: "relative", left: "-2rem", top: "-.3rem" }}>
           <Item>
-            <Typography sx={{fontFamily: "Lato", letterSpacing: ".1rem"}}>
-            © 2023 Blue Paradise
+            <Typography sx={{ fontFamily: "Lato", letterSpacing: ".1rem" }}>
+              © 2023 Blue Paradise
             </Typography>
           </Item>
         </Grid>
       </Grid>
       <Grid container spacing={2} sx={{ justifyContent: "flex-end" }}>
         <Grid item xs={3}>
-          <Item sx={{position: "relative", right: "-4rem", top: "-.8rem"}}>
+          <Item sx={{ position: "relative", right: "-4rem", top: "-.8rem" }}>
             <a href="www.facebook.com">
               <FacebookIcon sx={{ marginRight: "3px", position: "relative", top: "5px" }} />
               Facebook
@@ -49,15 +48,15 @@ export default function StickyFooter() {
           </Item>
         </Grid>
         <Grid item xs={3}>
-          <Item sx={{position: "relative", right: "-4rem", top: "-0.8rem"}}>
+          <Item sx={{ position: "relative", right: "-4rem", top: "-0.8rem" }}>
             <a href="www.youtube.com">
               <YouTubeIcon sx={{ marginRight: "3px", position: "relative", top: "5px" }} />
               YouTube
             </a>
           </Item>
         </Grid>
-        <Grid item xs={3} sx={{marginRight: "0"}}>
-          <Item sx={{position: "relative", right: "-2rem", top: "-0.8rem"}}>
+        <Grid item xs={3} sx={{ marginRight: "0" }}>
+          <Item sx={{ position: "relative", right: "-2rem", top: "-0.8rem" }}>
             <a href="www.twitter.com">
               <TwitterIcon sx={{ marginRight: "3px", position: "relative", top: "5px" }} />
               Twitter
